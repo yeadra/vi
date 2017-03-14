c498034d9a2112f9cc57ef0cd26462f0c898d813
yeadra <hspss90272@gmail.com>
2017-03-14 21:33:41 +0800

    Test for github

---
+++ 10minvi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int
+main(){
+	int k;
+	int x;
+	for
+(k=1;k<=9;k++){
+		cout<<"I love vi!!"<<endl;
+	}
+	cout<<"i love c++"<<endl;
+	out<<"i love c++"<<endl;
+
+return 0;
+}
+
